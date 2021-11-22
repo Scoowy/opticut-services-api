c15db684f69809ea47f66d640bc460387c6f3af1
Scoowy <gahonajuanjo@gmail.com>
2021-11-22 15:58:32 -0500

    Initial repository

---
+++ src/main/java/com/opticortes/OptiCutServicesAPI.java
@@ -0,0 +1,25 @@
+package com.opticortes;
+
+import spark.Spark;
+
+/**
+ * Class
+ *
+ * @author Scoowy
+ * @version 2021.11.22.1544
+ */
+public class OptiCutServicesAPI {
+    public static void main(String[] args) {
+        Spark.port(getPort());
+
+        Spark.get("/", (request, response) -> "Todo bien!");
+    }
+
+    static int getPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 8080;
+    }
+}
