93192393765ab48c41bd397fe04a4837da7d8ca0
Scoowy <gahonajuanjo@gmail.com>
2021-11-26 00:14:17 -0500

    BasicResponse class abstracted and Error and Ok response classes implemented

--- src/main/java/com/opticortes/utils/ErrorResponse.java
+++ src/main/java/com/opticortes/utils/ErrorResponse.java
@@ -1,49 +1,13 @@
 package com.opticortes.utils;
 
-import java.util.StringJoiner;
-
 /**
  * Class
  *
  * @author Scoowy
  * @version 2021.11.23.1804
  */
-public class ErrorResponse {
-    private int code;
-    private String message;
-    private final String status = "ERROR";
-
+public class ErrorResponse extends BasicResponse {
     public ErrorResponse(int code, String message) {
-        this.code = code;
-        this.message = message;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    @Override
-    public String toString() {
-        return new StringJoiner(", ", ErrorResponse.class.getSimpleName() + "[", "]")
-                .add("code=" + code)
-                .add("message='" + message + "'")
-                .add("status='" + status + "'")
-                .toString();
+        super(code, message, "ERROR");
     }
 }
---
+++ src/main/java/com/opticortes/utils/OkResponse.java
@@ -0,0 +1,13 @@
+package com.opticortes.utils;
+
+/**
+ * Class
+ *
+ * @author Scoowy
+ * @version 2021.11.25.2335
+ */
+public class OkResponse extends BasicResponse {
+    public OkResponse(int code, String message) {
+        super(code, message, "OK");
+    }
+}
